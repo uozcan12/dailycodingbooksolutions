b4aee50998940a49e4c6060a82fc0267d45746a6
Uğur Özcan <ugurozcan108@gmail.com>
2021-05-06 14:00:28 +0300

    Update problem1_1.py

--- problem1_1.py
+++ problem1_1.py
@@ -10,3 +10,20 @@ for i in range(N-1, -1, -1):
     B[i] = B[i] * next
     next = next * A[i]
 print(B)
+
+import numpy as np
+import time 
+
+def make_prod_list(a):
+    prod_list = []
+    for i in range(len(a)):
+        n=a[:]    
+        n.remove(a[i])
+        prod_list.append(np.prod(n))
+    return prod_list
+
+start = time.perf_counter()
+returned_function = make_prod_list(A)
+finish = time.perf_counter()
+total_time = f"Total time is : {finish-start}"
+print(total_time)
